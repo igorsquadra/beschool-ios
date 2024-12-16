b8814263fb332e5fe68dbc2d686c1cffd98b70f9
Igor Squadra <igor@igorsquadra.com>
2024-12-16 16:47:50 +0100

    Initial Commit

---
+++ beschool/beschoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  beschoolApp.swift
+//  beschool
+//
+//  Created by Igor Squadra on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct beschoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
